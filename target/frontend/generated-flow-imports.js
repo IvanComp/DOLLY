1e1ba691a5b276ee4ca33f4662216ab30c5426fe
Ivan <ivan.compagnucci@studenti.unicam.it>
2024-05-22 00:06:43 +0200

    Bugs fix

---
+++ target/frontend/generated-flow-imports.js
@@ -0,0 +1,34 @@
+export const addCssBlock = function(block, before = false) {
+ const tpl = document.createElement('template');
+ tpl.innerHTML = block;
+ document.head[before ? 'insertBefore' : 'appendChild'](tpl.content, document.head.firstChild);
+};
+
+import '@vaadin/common-frontend/ConnectionIndicator.js';
+import '@vaadin/polymer-legacy-adapter/style-modules.js';
+import '@vaadin/vaadin-lumo-styles/color.js';
+import '@vaadin/vaadin-lumo-styles/sizing.js';
+import '@vaadin/vaadin-lumo-styles/spacing.js';
+import '@vaadin/vaadin-lumo-styles/style.js';
+import '@vaadin/vaadin-lumo-styles/typography.js';
+import '@vaadin/vaadin-lumo-styles/vaadin-iconset.js';
+import 'Frontend/generated/jar-resources/lumo-includes.ts';
+let thisScript;
+const elements = document.getElementsByTagName('script');
+for (let i = 0; i < elements.length; i++) {
+ const script = elements[i];
+ if (script.getAttribute('type')=='module' && script.getAttribute('data-app-id') && !script['vaadin-bundle']) {
+  thisScript = script;
+  break;
+ }
+}
+if (!thisScript) {
+ throw new Error('Could not find the bundle script to identify the application id');
+}
+thisScript['vaadin-bundle'] = true;
+if (!window.Vaadin.Flow.fallbacks) { window.Vaadin.Flow.fallbacks={}; }
+const fallbacks = window.Vaadin.Flow.fallbacks;
+fallbacks[thisScript.getAttribute('data-app-id')] = {}
+fallbacks[thisScript.getAttribute('data-app-id')].loadFallback = function loadFallback() {
+ return import('./generated-flow-imports-fallback.js');
+}
